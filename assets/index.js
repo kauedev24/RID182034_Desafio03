b6b0274d59e2781eb6f879eb9652d953a4afc1ac
Kaue Oliveira <knascimento353@gmail.com>
2024-10-01 16:35:29 -0300

    v1.0 teste - do board de tarefas finalizado

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
+// pega a data de hoje DD/MM/AAAA
 const getTodayDate = () => {
     const date = new Date();
     const day = date.getDate();
@@ -7,23 +8,34 @@ const getTodayDate = () => {
     return `${day}/0${month}/${year}`;
 }
 
-const tasks = [];
+// pega a lista local
+const getTasksInLocalStorage = () => {
+    const localData = JSON.parse(window.localStorage.getItem("tasks"));
+    return localData ? localData : [];
+}
 
-const promiseToRemove = (cardID) => new Promise((resolve) => {
-    const cardItem = document.getElementById(cardID);
+// adiciona tarefas em lista local
+const addTasksInLocalStorage = (taskCards) => {
+    window.localStorage.setItem("tasks", JSON.stringify(taskCards));
+}
+
+// promessa de remoção da tarefa
+const promiseToRemove = (taskID) => new Promise((resolve) => {
+    const cardItem = document.getElementById(taskID);
 
     setTimeout(() => {
         resolve(cardItem.remove());
     }, 900);
 })
 
-const taskCompleted = async (cardID, titleID, buttonID) => {
-    const cardItem = document.getElementById(cardID);
+// ação botão "concluir tarefa" | retorna promessa de remoção
+const taskCompleted = async (taskID, titleID, buttonID) => {
+    const cardItem = document.getElementById(taskID);
     const title = document.getElementById(titleID);
     const button = document.getElementById(buttonID);
     const complete = document.createElement("img");
 
-    complete.src = "assets/img/complete.svg"; 
+    complete.src = "/assets/components/complete.svg"; 
 
     title.style = "text-decoration: line-through; color: rgb(143,152,168)";
 
@@ -32,10 +44,16 @@ const taskCompleted = async (cardID, titleID, buttonID) => {
 
     setTimeout(() => {
         cardItem.style.opacity = "0";
-        promiseToRemove(cardID);        
+        promiseToRemove(taskID);        
     }, 4000);
+
+    const tasks = getTasksInLocalStorage();
+    tasks[taskID-1].checked = true;
+    addTasksInLocalStorage(tasks);
+    createCompletedTaskCounter(tasks);
 }
 
+// cria o cartão
 const createCard = (task) => {
     const cardItem = document.createElement("li");
     const infoContainer = document.createElement("div");
@@ -45,11 +63,10 @@ const createCard = (task) => {
     const date = document.createElement("p");
     const button = document.createElement("button");
     const taskID = task.id;
-    const cardID = `card-${taskID}`;
     const buttonID = `done-btn-${taskID}`;
     const titleID = `title-${taskID}`;
 
-    cardItem.id = cardID;
+    cardItem.id = taskID;
     button.id = buttonID;
     title.id = titleID;
 
@@ -64,7 +81,7 @@ const createCard = (task) => {
     date.textContent = `Criado em: ${task.creationDate}`;
     button.textContent = "Concluir";
 
-    button.onclick = () => taskCompleted(cardID, titleID, buttonID);
+    button.onclick = () => taskCompleted(taskID, titleID, buttonID);
 
     cardItem.appendChild(infoContainer);
     infoContainer.appendChild(title);
@@ -76,6 +93,7 @@ const createCard = (task) => {
     return cardItem;
 }
 
+// cria um item na lista
 const createItemList = (task) => {
     const list = document.getElementById("task-list");
     const cardItem = createCard(task);
@@ -85,26 +103,53 @@ const createItemList = (task) => {
     return list;
 }
 
+// pega as informações do forms
 const getInfoTask = (event) => {
     event.preventDefault();
 
+    const tasks = getTasksInLocalStorage();
     const id = tasks.length === 0 ? 1 : tasks.length +1;
     const name = event.target.elements[0].value;
     const label = event.target.elements[1].value;
     const date = getTodayDate();
     const checked = false;
     const task = { id: id, name: name, label: label, creationDate: date, checked: checked };
+    const updateTasks = tasks.length === 0 ? [task] : [...tasks, task];
 
-    tasks.push(task);
+    addTasksInLocalStorage(updateTasks);
     createItemList(task);
+}
+
+// cria contador de tarefas concluidas
+const createCompletedTaskCounter = (tasks) => {
+    const footer = document.getElementById("footer");
+    const text = document.getElementById("footer-text");
+    
+    let tasksCompleted; 
+    if (text) tasksCompleted = text;
+    else {
+        const text = document.createElement("p");
+        text.id = "footer-text";
+        
+        tasksCompleted = text;
+    }
+
+    const counter = tasks.filter(({ checked }) => checked).length;
+    tasksCompleted.textContent = `${counter} tarefas concluidas`;
+    footer.appendChild(tasksCompleted);
+
+    return footer;
 }
 
 window.onload = function() {
 
     const form = document.getElementById("form-components");
     form.addEventListener("submit", getInfoTask);
-
+    
+    const tasks = getTasksInLocalStorage();
     tasks.forEach((task) => {
-        createItemList(task);
+        if (task.checked !== true) createItemList(task);
     })
+
+    createCompletedTaskCounter(tasks);
 }
